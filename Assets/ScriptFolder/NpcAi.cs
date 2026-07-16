102de97fb893d158bf74aeabbe640c4b33c0ad0d
k00281825 <K00281825@student.tus.ie>
2026-07-16 17:41:13 +0100

    ai update things got better have  line but new problem ith the navMeshSurface
    So this update doesnt work but end of day save

--- Assets/ScriptFolder/NpcAi.cs
+++ Assets/ScriptFolder/NpcAi.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NpcAi : MonoBehaviour
+public class UnitFormationAndMovementScript : MonoBehaviour
 {
 
     private NavMeshAgent agent;
     [SerializeField]
     private GameObject Target;
-
+    NavMeshPath path;
+    public Units[] units;
+    public int UnitAmount;
+    public float Spacing;
+    public float UnitSize;
+    private float middle;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +24,58 @@ public class NpcAi : MonoBehaviour
         {
             Debug.Log("No nav MeshAgent");
         }
+
+        UnitAmount = units.Length;
+        if(UnitAmount%2==0)
+        {
+            middle = (float)(( UnitAmount / 2) + 0.5);
+            middle -= 1;
+            Debug.Log(middle);
+        }
+        else
+        {
+            middle=UnitAmount/2;
+            Debug.Log(middle);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(Target.transform.position);
+        agent.CalculatePath(Target.transform.position,agent.path);
+       path= agent.path;
+        //Debug.Log(path.corners[0]);
+        
+        for(int i=0;i<units.Length;i++)
+        {
+            if(i<middle)
+            {
+               
+                float XPosOffset = (middle - i) * UnitSize;
+                Vector3 NewPosition = path.corners[0]-new Vector3(XPosOffset,0,0);
+
+                units[i].SetPosition(NewPosition);
+            }
+            else if(i==middle)
+            {
+
+                Vector3 NewPosition = path.corners[0];
+
+                units[i].SetPosition(NewPosition);
+            }
+            else if(i>middle)
+            {
+
+                float XPosOffset = (i-middle ) * UnitSize;
+                Vector3 NewPosition = path.corners[0] + new Vector3(XPosOffset, 0, 0);
+
+                units[i].SetPosition(NewPosition);
+
+
+            }
+
+           
+        }
     }
 }
---
+++ Assets/ScriptFolder/Units.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
+
+public class Units : MonoBehaviour
+{
+    private GameObject Soilder;
+    public Vector3 GoPosition;
+    private NavMeshAgent agent;
+    private NavMeshPath path;
+    public Vector3 CurrentVelocity;
+
+    public GameObject Target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+        {
+            Debug.Log("No nav MeshAgent");
+        }
+    }
+
+    public void SetPosition(Vector3 newPosition)
+    {
+        GoPosition = newPosition;
+        agent.SetDestination(GoPosition);
+        Target.transform.position = GoPosition;
+        // Debug.Log("Hey2");
+    }
+
+
+
+    void Update()
+    {
+        if (GoPosition != null)
+        {
+            //Debug.Log("Hey1");
+            CurrentVelocity = GetComponent<Rigidbody>().linearVelocity;
+            
+        }
+
+
+
+    }
+
+
+
+
+
+
+    }
